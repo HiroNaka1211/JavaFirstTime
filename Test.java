449e1fae8c982b0fcb151b44578cad3c05c831b3
HiroNaka1211 <N10978607@qut.edu.au>
2023-03-07 14:02:58 +0900

    Third commit

--- Test.java
+++ Test.java
@@ -1,16 +1,31 @@
 import java.util.Date;
 import java.awt.*;
+import java.util.Arrays;
+
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello World");
-        Date now = new Date();
-        now.getTime();  
-        // System.out.println(now);
-        Point point1 = new Point(1,2);
-        Point point2 = point1; 
-        point1.x = 2;
-        point2.x = 3;
-        System.out.println(point2);
-        System.out.println(point1);
+        // System.out.println("Hello World");
+        // Date now = new Date();
+        // now.getTime();  
+        // // System.out.println(now);
+        // Point point1 = new Point(1,2);
+        // Point point2 = point1; 
+        // point1.x = 2;
+        // point2.x = 3;
+        // System.out.println(point2);
+        // System.out.println(point1);
+
+        // String message = " Hello \"Mosh\"";
+
+        // String message = "C:\tWindows\\...";
+
+        // System.out.println(message.trim());
+        // System.out.println(message);
+
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(Arrays.toString(numbers));
     }
 }
